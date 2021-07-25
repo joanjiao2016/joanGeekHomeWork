5c879426442a473fcdf9f4350456ed483418fc30
joanjiao2016 <42462099+joanjiao2016@users.noreply.github.com>
2021-07-25 17:37:06 +0800

    Update MyInterface.java

--- GeekRPC/src/main/java/MyInterface.java
+++ GeekRPC/src/main/java/MyInterface.java
@@ -3,5 +3,5 @@ import org.apache.hadoop.ipc.VersionedProtocol;
 public interface MyInterface extends VersionedProtocol {
    long versionID = 1L;
     int add(int number1, int number2);
-    String getName(String stuNumber);
+    String findName(int studentId);
 }

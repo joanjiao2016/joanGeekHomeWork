e2cf7d0d970955648a87728ee61328fde32d924c
joanjiao2016 <42462099+joanjiao2016@users.noreply.github.com>
2021-07-25 17:38:03 +0800

    Update Client.java

--- GeekRPC/src/main/java/Client.java
+++ GeekRPC/src/main/java/Client.java
@@ -10,9 +10,9 @@ public class Client {
             MyInterface proxy = RPC.getProxy(MyInterface.class, 1L, new InetSocketAddress("127.0.0.1", 8082), new Configuration());
          //   int res = proxy.add(1, 2);
          //   System.out.println(res);
-            String name = proxy.getName("20210000000000");
+            String name = proxy.findName(20210000000000);
             System.out.println("student name of student number 20210000000000 is " + name);
-            String name1 = proxy.getName("20210123456789");
+            String name1 = proxy.findName(20210123456789);
             System.out.println("student name of student number 20210123456789 is " + name1);
         } catch (IOException e) {
             e.printStackTrace();
